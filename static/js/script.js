419141f05b11268283d05752f2e3557eb28e8aaf
Ana <gogia.413ana@gmail.com>
2025-03-25 09:33:38 +0530

    Added basic structure

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+function exploreResources() {
+    window.location.href = 'resources.html'; // Redirect to resources page
+}
+
+function getSupport() {
+    window.location.href = 'support.html'; // Redirect to support page
+}
+
+function playGames() {
+    window.location.href = 'games.html'; // Redirect to games page
+}
+
+function contactEmergency() {
+    window.location.href = 'emergency.html'; // Redirect to emergency support page
+}
